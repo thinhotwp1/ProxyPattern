daa9d4dbe7d67a8da42d4a8a17ff52b983157d30
ThinhLD <leduythinh20001@gmail.com>
2023-10-09 21:43:02 +0700

    update

--- ProxyPattern/src/proxyfakeip/ProxyPattern.java
+++ ProxyPattern/src/proxyfakeip/ProxyPattern.java
@@ -8,17 +8,21 @@ package proxyfakeip;
  *
  * @author Administrator
  */
-public class Main {
+public class ProxyPattern {
+    /**
+     * Remote Proxy: Khi bị chặn ở trang cado.vn ở Việt Nam thì tạo 1 proxy đứng chặn ở ngoài
+     * sẽ gọi qua proxy này tới trang cado.vn, vì proxy này ip nước ngoài nên trang cado.vn
+     * không bị chặn, proxy internet
+     */
+
 
     public static void main(String[] args) {
-        /* Remote Proxy: Đối tượng proxy sẽ làm việc với 
-        đối tượng thực sự nằm trên một máy chủ từ xa(fake ip) */
 
         System.out.println("===== Real Internet =====");
         Internet internetReal = new RealInternet();
         try {
             internetReal.connectTo("google.com");
-            internetReal.connectTo("bongda.com");
+            internetReal.connectTo("cado.vn");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
