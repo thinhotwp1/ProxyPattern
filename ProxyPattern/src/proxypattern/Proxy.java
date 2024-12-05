17a2247164574eb20f821c61e7c44db20d481b2b
ThinhLD <thinhld@telsoft.com.vn>
2024-12-05 12:54:06 +0700

    format

--- ProxyPattern/src/proxypattern/Proxy.java
+++ ProxyPattern/src/proxypattern/Proxy.java
@@ -9,12 +9,20 @@ public class Proxy implements RealObject {
 
     public void request() {
         // Log trước khi gọi request bằng object thật
-        System.out.println("Proxy: Logging before request");
+        beforeRequest();
 
         // Chuyển hướng cho object thật gọi request
         realObject.request();
 
         // Log sau khi gọi request bằng object thật
+        afterRequest();
+    }
+
+    private void afterRequest() {
         System.out.println("Proxy: Logging after request");
+    }
+
+    private void beforeRequest() {
+        System.out.println("Proxy: Logging before request");
     }
 }
